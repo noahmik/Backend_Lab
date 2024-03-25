ff5d06e06b241d4406ccc9aa87019e161d4e5c03
noahmik <s46422574@gmail.com>
2024-03-25 14:58:52 +0900

    ts 제네릭 정리

--- Lab.ts
+++ Lab.ts
@@ -1,33 +1,66 @@
-//클래스(class)
-abstract class Car {
-    readonly name: string = "car"; 
-    //name 앞에 #name 혹은 private 키워드를 붙이면 다른 class에서 호출 불가
-    //protected 키워드를 붙이면 자식 클래스에선 호출이 되지만 클래스인스턴스(z4.name)과 같은 걸론 접근불가
-    //public 키워드는 모두 가능
-    color: string;
-    static wheels = 4;
-    constructor(color:string, name){
-        this.color = color;
-    }
-    start(){
-        console.log("start");
-    }
-    abstract doSomething():void; //추상메서드
+// Generic
+//타입을 T로만 넣어두고 이후에 선언할때 정함
+function getSize<T>(arr: T[]):number{
+    return arr.length;
 }
 
-class Bmw extends Car {
-    constructor(color:string, name){
-        super(color, name);
-    }
-    showName(){
-        console.log(super.name);
-    }
-    doSomething(){
-        alert(3); //자식 클래스에서 구현
+const arr1 = [1,2,3];
+getSize<number>(arr1);
+
+const arr2 = ['a','b','c'];
+getSize<string>(arr2);
+
+const arr3 = [false,true,true];
+getSize<boolean>(arr3);
+
+const arr4 = [{},{name: 2},{a : "3"}];
+getSize<object>(arr4);
+
+
+interface Mobile<T> {
+    name : string;
+    price : number;
+    option : T;
+}
+
+const m1 : Mobile<object> = {
+    name : "s1",
+    price : 10000,
+    option : {
+        color : "red",
+        coupon : false
     }
 }
 
-const z4 = new Bmw("black","taeyoung"); //readonly에선 이렇게 변경
-console.log(z4.name);
-console.log(Car.wheels); //static 키워드에서는 Class명으로 호출 
-// z4.name = "blue" => readonly 키워드 내에서 사용 불가
+const m2 : Mobile<string> = {
+    name : "s1",
+    price : 10000,
+    option : "good"
+}
+
+
+interface User {
+    name: string,
+    age: number
+}
+
+interface Car {
+    name: string,
+    color: string
+}
+
+interface Book {
+    price : number
+}
+
+const user:User = {name:"a", age:10};
+const car:Car = {name:"bmw",color:"red"};
+const book:Book = {price:10000};
+
+function showName<T extends {name : string}>(data : T): string {
+    return data.name;
+}
+
+showName(user);
+showName(car);
+//showName(book); => book이 객체 내에 없음
